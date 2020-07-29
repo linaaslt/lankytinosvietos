33a580843927f14ab37bb680219a0ad3dfe0cb6f
linaaslt <linaaslt@gmail.com>
2020-07-29 10:02:29 +0300

    pirmas comitas

---
+++ src/main/java/ernadas/mokslai/lankytinosvietos/LankytinosvietosApplication.java
@@ -0,0 +1,13 @@
+package ernadas.mokslai.lankytinosvietos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LankytinosvietosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LankytinosvietosApplication.class, args);
+	}
+
+}
